3389ac70fb50c490d6427f9664177b8746046866
drehe <drehe@DESKTOP-4LN00DA>
2021-12-10 21:54:30 -0300

    Agregar archivos de proyecto.

---
+++ MegaHerdt.DbConfiguration/DbConfiguration/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using MegaHerdt.Models.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaHerdt.DbConfiguration.DbConfiguration
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<ApplicationUser> Users { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ MegaHerdt.Models/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MegaHerdt.Models.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Dni { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
