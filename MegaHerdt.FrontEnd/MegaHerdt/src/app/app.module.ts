c897f2b09afc10caa56c4d8cfaf7b42b36195918
franco dreher <dreheragustin@hotmail.com>
2022-04-15 21:09:31 -0300

    sort users styles

--- MegaHerdt.FrontEnd/MegaHerdt/src/app/app.module.ts
+++ MegaHerdt.FrontEnd/MegaHerdt/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AlertModule, AlertConfig } from 'ngx-bootstrap/alert';
 import { NgChartsModule } from 'ng2-charts';
 import { ColorPickerModule } from 'ngx-color-picker';
 import {MatSortModule} from '@angular/material/sort';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -201,6 +202,7 @@ import { BrandGraphicComponent } from './components/brands/brand-graphic/brand-g
     ColorPickerModule,
     NgChartsModule,
     MatSortModule,
+    MatAutocompleteModule,
     NgxStripeModule.forRoot('pk_test_51KOsMMLFz3iBJKgrLLDZC0HmvofwUbENMgupBtCnJ4Yo21bINMHBolzgbdg6lTB5bO3D9l9J6axnZUx6tE0nPHgD00yKyQXAax'),
   ],
   providers: [],
