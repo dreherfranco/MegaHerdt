0270e86e96d0ec7b889d0963bcc6b8444c17cda5
franco dreher <dreheragustin@hotmail.com>
2023-09-20 21:13:36 -0300

    ordenamiento en historiales de compras y de reparaciones

--- MegaHerdt.FrontEnd/MegaHerdt/src/app/components/purchases/purchase-record/purchase-record.component.ts
+++ MegaHerdt.FrontEnd/MegaHerdt/src/app/components/purchases/purchase-record/purchase-record.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sort } from '@angular/material/sort';
 import { Paginate } from 'src/app/models/Paginate/Paginate';
 import { Purchase } from 'src/app/models/Purchase/Purchase';
 import { UserDetail } from 'src/app/models/User/UserDetail';
@@ -13,6 +14,7 @@ import { StorageService } from 'src/app/services/storage/storage.service';
 export class PurchaseRecordComponent implements OnInit {
   purchases: Purchase[] = [];
   paginate: Paginate;
+  sortedData: Purchase[] = [];
 
   constructor(private _purchaseService: PurchaseService, private _storageService: StorageService) {
     this.paginate = new Paginate(1,4);
@@ -28,10 +30,35 @@ export class PurchaseRecordComponent implements OnInit {
       next: (result) =>{
         console.log(result)
         this.purchases = result;
+        this.sortedData = this.purchases.slice();
+
       },
       error: (err) => {
         console.log(err);
       }
     });
   }
+
+  sortData(sort: Sort) {
+    const data = this.purchases.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'date':
+          return compare(a.date, b.date, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+}
+
+function compare(a: number | boolean | string | Date | string[], b: number | boolean | string | Date | string[], isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
--- MegaHerdt.FrontEnd/MegaHerdt/src/app/components/reparations/reparation-record/reparation-record.component.ts
+++ MegaHerdt.FrontEnd/MegaHerdt/src/app/components/reparations/reparation-record/reparation-record.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sort } from '@angular/material/sort';
 import { Paginate } from 'src/app/models/Paginate/Paginate';
 import { Reparation } from 'src/app/models/Reparation/Reparation';
 import { UserDetail } from 'src/app/models/User/UserDetail';
@@ -13,6 +14,7 @@ import { StorageService } from 'src/app/services/storage/storage.service';
 export class ReparationRecordComponent implements OnInit {
   reparations: Array<Reparation>;
   paginate: Paginate;
+  sortedData: Reparation[] = [];
 
   constructor(private _storageService: StorageService,
     private _reparationService: ReparationService) { 
@@ -32,6 +34,8 @@ export class ReparationRecordComponent implements OnInit {
           console.log("error al obtener reparaciones");
         } else {
           this.reparations = response;
+          this.sortedData = this.reparations.slice();
+
           console.log(response)
         }
       },
@@ -41,4 +45,42 @@ export class ReparationRecordComponent implements OnInit {
     });
   }
 
+  sortData(sort: Sort) {
+    const data = this.reparations.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+          case 'employeeEmail':
+            return compare(a.employee.email, b.employee.email, isAsc);
+          case 'clientEmail':
+            return compare(a.client.email, b.client.email, isAsc);
+          case 'amount':
+            return compare(a.amount, b.amount, isAsc);
+          case 'totalArticleAmount':
+            return compare(a.totalArticleAmount, b.totalArticleAmount, isAsc);
+          case 'total':
+            return compare((a.totalArticleAmount + a.amount), (b.totalArticleAmount + b.amount), isAsc);
+          case 'date':
+            return compare(a.date, b.date, isAsc);
+          case 'reparationState':
+            return compare(a.reparationState.name, b.reparationState.name, isAsc);
+          case 'billNumber':
+            return compare(a.bill.number, b.bill.number, isAsc);
+          case 'billType':
+            return compare(a.bill.type, b.bill.type, isAsc);
+          default:
+            return 0;
+      }
+    });
+  }
+
+}
+
+function compare(a: number | boolean | string | Date | string[], b: number | boolean | string | Date | string[], isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
